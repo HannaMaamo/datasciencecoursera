1a5cc3f832cb6af34a6a91b287997672d4a80550
HannaMaamo <30763168+HannaMaamo@users.noreply.github.com>
2017-08-27 21:13:01 -0700

    Add files via upload

---
+++ Assignment 2 - Lexical Scoping.R
@@ -0,0 +1,22 @@
+makeCacheMatrix <- functon(x = matrix()){
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL 
+  }
+  get <- function()x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse < function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+cacheSolve <- function (x,...){
+    inv <- x$getinverse()
+    if(!is.null(inv)){
+      message("Getting cached data.")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+}
